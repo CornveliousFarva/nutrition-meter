d0b375f66e022120c8c7de3c2c2b29f3bf6aeaec
Greg Schuman <gcschuman@hotmail.com>
2023-12-11 12:38:53 -0800

    Added the imports in the script.

---
+++ src/NutritionMeter.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import "./NutritionMeter.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
+import { 
+  faEdit, 
+  faTrashAlt, 
+  faUtensils, 
+  faPlus, 
+  faMinus, 
+  faTimes, 
+} from "@fortawesome/free-solid-svg-icons";
+
