f3093374f74d86a34b5f3089fc83a76a01162382
Greg Schuman <gcschuman@hotmail.com>
2023-12-14 12:30:53 -0800

    Added button html.

--- src/NutritionMeter.jsx
+++ src/NutritionMeter.jsx
@@ -114,21 +114,127 @@ const NutritionMeter = () => {
     };
 
     return (
-        <div className="bg-green-200 min-h-screen">
-            <div className="container mx-auto p-4">
-                <h1 className="text-3x1 font-semibold text-center mb-4">
-                    Nutrition Meter
-                </h1>
-                {showWarning && (
-                    <div className="bg-red-500 text-white p-2 rounded-md text-center">
-                        <FontAwesomeIcon icon={faTimes} className="mr-2" /> 
+        <div className="bg-green-200 min-h-screen"> 
+      <div className="container mx-auto p-4"> 
+        <h1 className="text-3xl font-semibold text-center mb-4"> 
+          GeeksforGeeks Nutrition Meter 
+        </h1> 
+        {showWarning && ( 
+          <div className="bg-red-500 text-white p-2 rounded-md text-center mb-4"> 
+            <FontAwesomeIcon icon={faTimes} className="mr-2" /> 
             Total calories exceed recommended limit (1000 calories)! 
-                )}
-                </div>
-            </div>
-
+          </div> 
+        )} 
+        <div className="mb-4"> 
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4"> 
+            <div> 
+              <input 
+                type="text"
+                placeholder="Item Name"
+                className={`w-full py-2 px-3 border rounded-md focus:outline-none  
+                focus:ring focus:border-blue-300 ${ 
+                  inputError && !newItem.name ? "border-red-500" : ""
+                }`} 
+                style={inputError && !newItem.name ? inputErrorStyle : {}} 
+                value={newItem.name} 
+                onChange={(e) => 
+                  setNewItem({ ...newItem, name: e.target.value }) 
+                } 
+              /> 
+            </div> 
+            <div> 
+              <input 
+                type="number"
+                placeholder="Calories"
+                className={`w-full py-2 px-3 border rounded-md  
+                focus:outline-none focus:ring focus:border-blue-300 ${ 
+                  inputError && newItem.calories < 0 ? "border-red-500" : ""
+                }`} 
+                style={inputError && newItem.calories < 0 ? inputErrorStyle : {}} 
+                value={newItem.calories} 
+                onChange={(e) => 
+                  setNewItem({ ...newItem, calories: e.target.value }) 
+                } 
+              /> 
+            </div> 
+            <div> 
+              <input 
+                type="number"
+                placeholder="Protein (g)"
+                className={`w-full py-2 px-3 border rounded-md focus:outline-none  
+                focus:ring focus:border-blue-300 ${ 
+                  inputError && newItem.protein < 0 ? "border-red-500" : ""
+                }`} 
+                style={inputError && newItem.protein < 0 ? inputErrorStyle : {}} 
+                value={newItem.protein} 
+                onChange={(e) => 
+                  setNewItem({ ...newItem, protein: e.target.value }) 
+                } 
+              /> 
+            </div> 
+            <div> 
+              <input 
+                type="number"
+                placeholder="Carbs (g)"
+                className={`w-full py-2 px-3 border rounded-md focus:outline-none  
+                focus:ring focus:border-blue-300 ${ 
+                  inputError && newItem.carbs < 0 ? "border-red-500" : ""
+                }`} 
+                style={inputError && newItem.carbs < 0 ? inputErrorStyle : {}} 
+                value={newItem.carbs} 
+                onChange={(e) => 
+                  setNewItem({ ...newItem, carbs: e.target.value }) 
+                } 
+              /> 
+            </div> 
+            <div> 
+              <input 
+                type="number"
+                placeholder="Fat (g)"
+                className={`w-full py-2 px-3 border rounded-md focus:outline-none  
+                focus:ring focus:border-blue-300 ${ 
+                  inputError && newItem.fat < 0 ? "border-red-500" : ""
+                }`} 
+                style={inputError && newItem.fat < 0 ? inputErrorStyle : {}} 
+                value={newItem.fat} 
+                onChange={(e) => 
+                  setNewItem({ ...newItem, fat: e.target.value }) 
+                } 
+              /> 
+            </div> 
+            <div className="col-span-2 sm:col-span-1"></div> 
+            
+        <div className="mt-3 flex justify-between">
+        <div className="mt-3 flex justify-between"> 
+                
+                {editItem ? ( 
+                  <button 
+                    className="bg-blue-500 text-white p-3 rounded-md hover:bg-blue-600 mb-4  
+                    font-semibold focus:outline-none text-xs"
+                    onClick={updateItemFunction} 
+                  > 
+                    Update Item 
+                  </button> 
+                ) : ( 
+                  <button 
+                    className="bg-green-600 text-white p-3 rounded-md hover:bg-green-700 mb-4  
+                    font-semibold focus:outline-none text-xs"
+                    onClick={addNutritionItem} 
+                  > 
+                    Add Item 
+                  </button> 
+                )} 
+                <button 
+                  className="bg-red-600 text-white p-3 rounded-md font-semibold mb-4 hover:bg-red-700  
+                  focus:outline-none text-xs"
+                  onClick={removeAllItems} 
+                > 
+                  Clear All 
+                </button> 
+              </div> 
+          </div> 
 
         </div>
-
+    </div>
     )
 }
